c711ec323770f811972993a940d80590a8b4273a
ultimatefergal <fernando.galarcio@gmail.com>
2024-12-16 01:17:27 -0500

    Set up authentication system

---
+++ src/components/profile.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function Profile() {
+  const session = useSession();
+
+  if (session.data?.user) {
+    return (
+      <div>
+        From client: user is signed in {JSON.stringify(session.data.user)}
+      </div>
+    );
+  }
+  return <div>From Client: user is NOT signed in</div>;
+}
