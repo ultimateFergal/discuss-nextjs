62cbda7dedb68b82b396079c61fe7fec31b1b68c
ultimatefergal <fernando.galarcio@gmail.com>
2025-01-12 12:05:57 -0500

    Add spinner button, useactionstate

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,11 +1,17 @@
 "use server";
 
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
 
 const createTopicSchema = z.object({
   name: z
     .string()
-    .min(1)
+    .min(3)
     .max(255)
     .regex(/^[a-z]+$/, {
       message: "Must be lowercase letters or dashes without spaces",
@@ -13,13 +19,64 @@ const createTopicSchema = z.object({
   description: z.string().min(10).max(255),
 });
 
-export async function createTopic(formData: FormData) {
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
   });
+  await new Promise((resolve) => setTimeout(resolve, 2500));
+  const session = await auth();
+
+  if (!session?.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    // console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
   }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["An unknown error occurred"],
+        },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
---
+++ src/components/common/form-button.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+
+interface FormButtonProps {
+  children: React.ReactNode;
+  isLoading: boolean;
+}
+
+export default function FormButton({ children, isLoading }: Readonly<FormButtonProps>) {
+  return (
+    <Button type="submit" isLoading={isLoading}>
+      {children}
+    </Button>
+  );
+}
