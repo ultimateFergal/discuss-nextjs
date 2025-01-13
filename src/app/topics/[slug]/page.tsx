ff2ec7e3528431dcc84d4dbc7765a081d54dbe8d
ultimatefergal <fernando.galarcio@gmail.com>
2025-01-12 20:54:15 -0500

    Create post

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,7 +1,24 @@
+import PostCreateForm from "@/components/posts/post-create-form";
 
+interface TopicShowPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
 
-export default function TopicShowPage() {
+export default async function TopicShowPage({
+  params,
+}: Readonly<TopicShowPageProps>) {
+  const { slug } = await params;
   return (
-    <div>TopicShowPage</div>
-  )
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm slug={slug}/>
+      </div>
+    </div>
+  );
 }
