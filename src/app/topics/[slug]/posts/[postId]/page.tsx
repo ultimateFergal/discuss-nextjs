c2ef0a292b2817739a89899d5be75d86918dea8b
ultimatefergal <fernando.galarcio@gmail.com>
2025-01-14 00:13:22 -0500

    create and show posts and comments

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import PostShow from "@/components/posts/post-show";
+import paths from "@/paths";
+import Link from "next/link";
+import CommentList from "@/components/comments/comment-list";
 
-export default function PostShowPage() {
+interface PostShowPageProps {
+  params: Promise<{
+    postId: string;
+    slug: string;
+  }>;
+}
+
+export default async function PostShowPage({
+  params,
+}: Readonly<PostShowPageProps>) {
+  const { postId, slug } = await params;
+
   return (
-    <div>PostShowPage</div>
-  )
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"<"} Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList postId={postId} />
+    </div>
+  );
 }
---
+++ src/db/queries/posts.ts
@@ -0,0 +1,26 @@
+import type { Post } from "@prisma/client";
+import { db } from "@/db";
+
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
+
+export function fetchPostsByTopicSlug(
+  topicSlug: string
+): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: { topic: { slug: topicSlug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
